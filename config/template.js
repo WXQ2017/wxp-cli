f01307fd9c96a44248f4d1bbd94d40110178086b
wxq <wxq@dra100.com>
2019-07-29 19:50:39 +0800

    news: 新增构建页面和组件指令

---
+++ config/template.js
@@ -0,0 +1,39 @@
+const path=require('path')
+module.exports={
+  bid:{
+    helpers:{
+      toLowercase:(str)=>str.toLocaleLowerCase()
+    },
+    page: {
+      output:path.join(__dirname,'src/pages'),
+      templates:[
+        {
+          name:'PageSample',
+          src:path.join(__dirname,'templates/pages/PageSample'),
+          prompts:[]
+        },
+        {
+          name:'PageReducer',
+          src:path.join(__dirname,'templates/pages/PageReducer'),
+          prompts:[]
+        }
+      ]
+    },
+    component:{
+      output:path.join(__dirname,'src/components'),
+      templates:[
+        {
+          name:'ComSample',
+          src:path.join(__dirname,'templates/components/ComSample'),
+          prompts:[
+            {
+              type:'input',
+              name:'content',
+              message:'the content of component'
+            }
+          ]
+        }
+      ]
+    }
+  }
+}
